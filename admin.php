5f3dc2b82f8e6066b28359e2b71f63997d0a0385
AAAblaze <56286416+AAAblaze@users.noreply.github.com>
2021-04-09 18:49:03 -0400

    Add files via upload

---
+++ admin.php
@@ -0,0 +1,24 @@
+<?php
+
+?>
+
+
+<html lang="en">
+<meta charset="UTF-8">
+<meta name="Author" content="Kangxuan Ye">
+<head>
+    <title>Admin</title>
+</head>
+
+<body>
+    <h2> Successfully jumped </h2>
+    <table>
+        <tr>
+            <td><a href="inventory.php">Inventory</a></td>
+            <td><a href="people_dosed.php">People Dosed</a></td>
+            <td><a href="in_schedule.php">In-Schedule</a></td>
+            <td><a href="waitlist.php">Waitlist</a></td>
+        </tr>
+    </table>
+</body>
+</html>
