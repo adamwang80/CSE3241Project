58acb7ed12d41b341ce12cb13db8010f1ddefe75
AAAblaze <56286416+AAAblaze@users.noreply.github.com>
2021-04-12 16:51:18 -0400

    Update inventory.php

--- inventory.php
+++ inventory.php
@@ -1,5 +1,25 @@
 <?php
-
+    include("conn.php"); //connection to database
+    $sql = "SELECT Manufacturer AS Manufacturer, SUM(Quantity_Of_Doses) AS Total_Received, SUM(Quantity_Of_Available) AS Total_Available, SUM(Quantity_Of_Dosed) AS Total_Distributed, SUM(Quantity_Of_Expired) AS Total_Expired FROM BATCHES GROUP BY Manufacturer";
+    $query = mysqli_query($conn, $sql);
+    echo "<table style= \"BORDER-COLLAPSE: collapse\" borderColor=\"#111111\" cellSpacing=\"0\" cellPadding=\"2\" border=\"1\">";
+    echo "  <tr>";
+    echo "      <td> Manufacturer </td>";
+    echo "      <td> Total received vaccines </td>";
+    echo "      <td> Total available vaccines  </td> ";
+    echo "      <td> Total distributed vaccines  </td> ";
+    echo "      <td> Total expired vaccines  </td> ";
+    echo "  </tr>";
+    while($row = mysqli_fetch_assoc($query)){
+        echo "  <tr>";
+        echo "      <td> ".$row['Manufacturer']." </td>";
+        echo "      <td> ".$row['Total_Received']." </td>";
+        echo "      <td> ".$row['Total_Available']." </td>";
+        echo "      <td> ".$row['Total_Distributed']." </td>";
+        echo "      <td> ".$row['Total_Expired']." </td>";
+        echo "  </tr>";
+    }
+    echo "</table>"
 ?>
 
 
